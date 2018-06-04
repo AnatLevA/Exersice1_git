41c8d012040ca74aea1d6f72f3144fd255b65454
AnatLevA <anatwork123@gmail.com>
2018-06-04 11:17:58 +0300

    "recursive implementation"

--- factorial.py
+++ factorial.py
@@ -1,13 +1,12 @@
-
 num = input("please enter a positive real number\n")
 
 def factorial_func(x):
-   max_range=x+1
-   count=1
-   for i in range(1,max_range):
-       count=count*i
+   if x==0:
+       return 1
+   else:
+       return x*factorial_func(x-1)
 
-   return count
+
 
 outcome=factorial_func(num)
 print("the factorial of the number " + str(num) + " is " + str(outcome))
